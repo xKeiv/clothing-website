6330fbe5058ec84f415907c679ca4f963319798f
JacW <94559281+xKeiv@users.noreply.github.com>
2023-01-14 15:34:05 +0100

    Hero Component [2]
    - Added Typography Tailwind Classes
    - Make it better visually
    - Added some animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,24 @@ import React from 'react'
 const Hero = () => {
   return (
     <div className='text-[#DE402F]'>
-      <div className='text-5xl text-center font-bold p-4 m-4'>Why are we different?</div>
-      <div className='grid grid-cols-4 gap-8'>
-        <div>
+      <div className='text-5xl text-center font-bold p-4 m-4'>Why are we <span className='text-[#CEDE2F] animate-pulse'>different</span>?</div>
+      <div className='grid grid-cols-4 gap-2'>
+        <p className="p-1 m-1 text-justify tracking-tight">
+          <h1 className='text-xl font-bold'>Quality over quantity.</h1>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh velit, condimentum eget posuere et, suscipit nec elit. In dignissim nunc vitae ante fringilla interdum. Aenean semper nibh vitae fermentum egestas. Fusce tristique, turpis placerat hendrerit congue, leo arcu condimentum dolor, vitae rhoncus odio leo et quam. Vestibulum nibh turpis, maximus nec ante vel, vehicula volutpat mi. Sed sed diam tristique, auctor erat vel, laoreet purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean eget dui dui. Mauris lacinia enim arcu, at viverra justo auctor quis. Suspendisse commodo laoreet mauris, vitae consectetur est elementum in. Nunc pretium orci et arcu pretium lobortis.
-        </div>
-        <div>
+        </p>
+        <p className="p-1 m-1 text-justify tracking-tight">
+          <h1 className='text-xl font-bold'>Ethically Selected Manufacturers</h1>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh velit, condimentum eget posuere et, suscipit nec elit. In dignissim nunc vitae ante fringilla interdum. Aenean semper nibh vitae fermentum egestas. Fusce tristique, turpis placerat hendrerit congue, leo arcu condimentum dolor, vitae rhoncus odio leo et quam. Vestibulum nibh turpis, maximus nec ante vel, vehicula volutpat mi. Sed sed diam tristique, auctor erat vel, laoreet purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean eget dui dui. Mauris lacinia enim arcu, at viverra justo auctor quis. Suspendisse commodo laoreet mauris, vitae consectetur est elementum in. Nunc pretium orci et arcu pretium lobortis.
-        </div>
-        <div>
+        </p>
+        <p className="p-1 m-1 text-justify tracking-tight">
+          <h1 className='text-xl font-bold'>Your voice matters!</h1>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh velit, condimentum eget posuere et, suscipit nec elit. In dignissim nunc vitae ante fringilla interdum. Aenean semper nibh vitae fermentum egestas. Fusce tristique, turpis placerat hendrerit congue, leo arcu condimentum dolor, vitae rhoncus odio leo et quam. Vestibulum nibh turpis, maximus nec ante vel, vehicula volutpat mi. Sed sed diam tristique, auctor erat vel, laoreet purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean eget dui dui. Mauris lacinia enim arcu, at viverra justo auctor quis. Suspendisse commodo laoreet mauris, vitae consectetur est elementum in. Nunc pretium orci et arcu pretium lobortis.
-        </div>
-        <div>
+        </p>
+        <p className="p-1 m-1 text-justify tracking-tight">
+          <h1 className='text-xl font-bold'>We take care about our Mother Earth!</h1>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam nibh velit, condimentum eget posuere et, suscipit nec elit. In dignissim nunc vitae ante fringilla interdum. Aenean semper nibh vitae fermentum egestas. Fusce tristique, turpis placerat hendrerit congue, leo arcu condimentum dolor, vitae rhoncus odio leo et quam. Vestibulum nibh turpis, maximus nec ante vel, vehicula volutpat mi. Sed sed diam tristique, auctor erat vel, laoreet purus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean eget dui dui. Mauris lacinia enim arcu, at viverra justo auctor quis. Suspendisse commodo laoreet mauris, vitae consectetur est elementum in. Nunc pretium orci et arcu pretium lobortis.
-        </div>
+        </p>
       </div>
     </div>
   )
