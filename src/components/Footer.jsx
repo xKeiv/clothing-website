467245ce7cba92df615e5ce38c9a811ec532eea8
JacW <94559281+xKeiv@users.noreply.github.com>
2023-01-20 19:04:05 +0100

    Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='bg-[#DE972F] full-w h-11 mx-auto py-auto px-4 text-[#DE402F]'>
-      Project 
+    <div className='bg-[#DE972F] grid grid-cols-1 mx-auto py-10 px-3 gap-4 text-[#DE402F]'>
+      <h1 className='text-2xl'>About this project</h1>
+      This site has been created with the help of TailwindCSS, ReactJS and many others useful libraries and packages
     </div>
   )
 }
